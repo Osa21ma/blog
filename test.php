8202714d017de0d18dfa0b2633a55328820f8c86
Osa21ma <Osama.abdelfattah21@gmail.com>
2025-06-17 21:56:43 +0300

    first commit

---
+++ test.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+
+localization (english , arabic )
+
+
+0-file ar , file en done
+
+
+
+
+1-1-link lang-> file store press lang -> session(
+)  done
+
+1-2- redirect the same palce  server done 
+
+2- data of lang 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- oop
+
+
+
+*/
+
+
+
+
+/*
+
+limit $limit offset $offset
+
+$limit 2;
+
+$offset  = (page-1)*limit
+
+page   limit   offset
+1        2       0
+2        2       2
+3        2       4
+
+
+
+
+
+*/
